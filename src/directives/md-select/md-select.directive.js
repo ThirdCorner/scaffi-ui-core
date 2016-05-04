9a3eb814d11c0a5cd5c37c7086c054671accbcb5
Joseph Blake <jblake@thirdcorner.com>
2016-05-04 16:02:44 -0500

    Took off restriction for md-select because of plugin issue

--- src/directives/md-select/md-select.directive.js
+++ src/directives/md-select/md-select.directive.js
@@ -23,9 +23,6 @@ class MdSelect {
 
 	compile(element, attrs){
 		
-		if(element.parent()[0].tagName != 'MD-INPUT-CONTAINER') {
-			throw new Error("Your input must be nested in an md-input-container.");
-		}
 		
 		var messageContainer = null;
 		if(attrs.name) {
