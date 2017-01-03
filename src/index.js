153d92ab1adc6194beb597823d17627062f307b3
Joseph Blake <jblake@thirdcorner.com>
2017-01-03 16:48:26 -0600

    Added fix to ie that doesn't support object.constructor.name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+/*
+	Browser Support Fillers
+ */
+
+/**
+ * Hack in support for Function.name for browsers that don't support it.
+ * IE, I'm looking at you.
+ **/
+if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+	Object.defineProperty(Function.prototype, 'name', {
+		get: function() {
+			var funcNameRegex = /function\s([^(]{1,})\(/;
+			var results = (funcNameRegex).exec((this).toString());
+			return (results && results.length > 1) ? results[1].trim() : "";
+		},
+		set: function(value) {}
+	});
+}
+
+
 import angular from "angular";
 import 'angular-aria';
 import 'angular-animate';
